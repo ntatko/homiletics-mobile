9e32ddd3b7b73ddc64fc4519c740f79786ac3721
Noah Tatko <tatkon@rbtrainers.com>
2026-01-03 22:30:46 -0600

    oopsie

---
+++ lib/classes/hive/user.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+part 'user.g.dart';
+
+@HiveType(typeId: 0)
+class User {
+  @HiveField(0)
+  late String id;
+
+  @HiveField(1)
+  String preferredVersion;
+
+  @HiveField(2)
+  String preferredLanguage;
+
+  User({this.preferredVersion = "web", this.preferredLanguage = "en"}) {
+    const uuid = Uuid();
+
+    // these are always set automatically & cannot be changed after initialized
+    id = uuid.v4();
+  }
+
+  String getHiveKey() => id;
+}
